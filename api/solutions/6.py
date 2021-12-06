26dec1cbe94cf2e825ead27328c752705db21446
chase.culpepepper@ethosgroup.com <chase.culpepper@ethosgroup.com>
2021-12-06 15:34:55 -0600

    Fixing for @marshall

--- api/solutions/6.py
+++ api/solutions/6.py
@@ -8,24 +8,13 @@ from collections import Counter
 pp = pprint.PrettyPrinter(indent=4)
 
 def solve_part_one(file='../solutions/inputs/6.in'):
-    fish = load_file(file)
 
-    fish_countdown = [0 for i in range(9)]
-
-    keys = Counter(fish).keys()
-    values = Counter(fish).values()
-    for i in range(len(keys)):
-        fish_countdown[keys[i]] = values[i]
-
-    days = 80
-    for day in range(days):
-        new_fish = fish_countdown.pop(0)
-        fish_countdown[6] += new_fish
-        fish_countdown.append(new_fish) 
-
-    return sum(fish_countdown)
+    return get_population_after(file, 80)
 
 def solve_part_two(file='../solutions/inputs/6.in'):
+    return get_population_after(file, 256)
+
+def get_population_after(file, days):
     fish = load_file(file)
 
     fish_countdown = [0 for i in range(9)]
@@ -35,7 +24,6 @@ def solve_part_two(file='../solutions/inputs/6.in'):
     for i in range(len(keys)):
         fish_countdown[keys[i]] = values[i]
 
-    days = 256
     for day in range(days):
         new_fish = fish_countdown.pop(0)
         fish_countdown[6] += new_fish
